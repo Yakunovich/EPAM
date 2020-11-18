a35d710ed5b1176a0f766dbaea1085da5c0d8588
Yakunovich <54987759+Yakunovich@users.noreply.github.com>
2020-11-18 21:03:59 +0300

    Update AirportTest.java

--- CleanCode/Java/src/test/java/AirportTest.java
+++ CleanCode/Java/src/test/java/AirportTest.java
@@ -32,21 +32,22 @@ public class AirportTest {
     );
 
     private static PassengerPlane planeWithMaxPassengerCapacity = new PassengerPlane("Boeing-747", 980, 16100, 70500, 242);
-    
+
     @Test
-    public void experimentalPlanesHasClassificationLevelHigherThanUnclassifiedTest(){
-        Assert.assertTrue(new Airport(planes).getUnclassifiedExperimentalPlanes().size() == 0);
-    }
-    @Test
-    public void airportHasAtLeastOneTransportMilitaryPlaneTest() {
-        Assert.assertTrue(new Airport(planes).getTransportMilitaryPlanes().stream().anyMatch(militaryPlane ->
-                militaryPlane.getType()==MilitaryTypes.TRANSPORT));
+    public void getTransportMilitaryPlanesTest() {
+        Airport airport = new Airport(planes);
+        Assert.assertTrue(airport.getTransportMilitaryPlanes().size() > 0);
     }
 
     @Test
     public void getPassengerPlaneWithMaxCapacityTest() {
-        Assert.assertTrue(new Airport(planes).getPassengerPlaneWithMaxPassengersCapacity()
-                .equals(planeWithMaxPassengerCapacity));
+        Airport airport = new Airport(planes);
+        Assert.assertTrue(airport.getPassengerPlaneWithMaxPassengersCapacity().equals(planeWithMaxPassengerCapacity));
     }
-    
+
+    @Test
+    public void hasAtLeastOneBomberInMilitaryPlanesTest() {
+        Airport airport = new Airport(planes);
+        Assert.assertTrue(airport.getBomberMilitaryPlanes().size() > 0);
+    }
 }
